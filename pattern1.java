2f127076e170b7c2b6f22dece101a8426c3f0505
Sumit Desai <67994487+sumitdesai9500@users.noreply.github.com>
2023-04-30 20:40:41 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,14 @@
+// Soild Rectangle
+
+public class patterns {
+    public static void main(String args[]) {
+        int i, j;
+
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
